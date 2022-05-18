254f17b066f0252de0c37abdb262632355739ab0
Ziheng Liu <ziheng@Zihengs-MacBook-Pro.local>
2022-05-18 16:55:56 -0700

    Init tage

--- src/predictor.c
+++ src/predictor.c
@@ -56,6 +56,48 @@ uint8_t *tour_l_pattern; // a table with 2^TOUR_L_HISTORY entries, and each entr
 int TOUR_C_ENTRY = 4 * 1024;
 uint8_t *tour_c_choice; // a table with TOUR_C_ENTRY entries, and each entry uses 2 bits
 
+
+//-------tage--------
+// design based on an article https://ssine.ink/en/posts/tage-predictor/
+
+// base predictor part
+const int TAGE_BASE_ENTRY = 1024 * 1024;
+uint8_t *tage_base_gshare;
+uint64_t tage_base_history;
+
+// tagged predictor part
+const int TAGE_COMP_NUM = 5; // there are 5 components, and each component is a tagged predictor
+const int TAGE_TAG_LEN = 10;
+const int TAGE_COMP_ENTRY = 256; // number of entries in each component
+const uint8_t TAGE_HISTORY_LEN[TAGE_COMP_NUM] = {80, 40, 20, 10, 5}; // the length of history used by each component is different
+const int TAGE_G_HISTORY_LEN = 128;
+
+uint8_t tage_g_history[TAGE_G_HISTORY_LEN]; // a very long history, and each component will use part of it
+uint32_t tage_l_history;
+
+typedef struct tage_comp_entry {
+  uint16_t tag; // another hash of pc and history
+  uint8_t choice; // 2-bit state machine
+  uint8_t usage; // whether this entry is useful
+} comp_entry;
+
+typedef struct tage_history {
+  uint8_t len_history;
+  uint8_t len_targe;
+  uint64_t compressed;
+} history;
+
+typedef struct tage_comp {
+  comp_entry entry[TAGE_COMP_ENTRY];
+  history index;
+  history first_tag;
+  history second_tag;
+  uint8_t len_history;
+} comp;
+
+comp tage_comp_list[TAGE_COMP_NUM];
+uint16_t tage_comp_entry_index[TAGE_COMP_NUM]; // store the entry index in tage_comp
+
 //------------------------------------//
 //        Predictor Functions         //
 //------------------------------------//
@@ -202,6 +244,18 @@ tournament_g_predict(uint32_t pc) {
   }
 }
 
+void print_backward_binary(uint16_t input) {
+  while (input) {
+    if (input & 1 )
+        printf("1");
+    else
+        printf("0");
+
+    input = input >> 1;
+  }
+  printf("\n");
+}
+
 uint8_t 
 tournament_l_predict(uint32_t pc) {
   // get lower bits of pc
@@ -209,6 +263,10 @@ tournament_l_predict(uint32_t pc) {
   // get local history of this branch
   uint16_t local_history = tour_l_history[pc_lower_bits];
   local_history = local_history & (my_pow2(TOUR_L_HISTORY) - 1);
+  // if (pc == 4259562) {
+  //     print_backward_binary(local_history);
+  // }
+  
   // get prediction
   switch(tour_l_pattern[local_history]) {
     case WN:
@@ -344,6 +402,123 @@ cleanup_tournament() {
 
 //---------End of Tournament
 
+
+//---------TAGE functions
+
+// utils
+
+// init
+
+void init_tage() {
+
+  // base
+  tage_base_history = 0;
+  tage_base_gshare = (uint8_t*)malloc(TAGE_BASE_ENTRY * sizeof(uint8_t));
+  int i = 0;
+  for(i = 0; i< TAGE_BASE_ENTRY; i++) {
+    tage_base_gshare[i] = WN;
+  }
+
+  // component global
+  for(i = 0; i< TAGE_G_HISTORY_LEN; i++) {
+    tage_g_history[i] = 0;
+  }
+  tage_l_history = 0;
+
+  // component each
+  for(i = 0; i< TAGE_COMP_NUM; i++) {
+    tage_comp_entry_index[i] = 0;
+
+    int j = 0;
+    for(j = 0; j< TAGE_COMP_ENTRY; j++) {
+      tage_comp_list[i].entry[j].choice = 00;
+      tage_comp_list[i].entry[j].tag = 0; // 10 0s 
+      tage_comp_list[i].entry[j].usage = 00;
+    }
+
+    tage_comp_list[i].first_tag.len_history = TAGE_HISTORY_LEN[i];
+    tage_comp_list[i].first_tag.compressed = 0;
+    tage_comp_list[i].first_tag.len_targe = (uint8_t) (TAGE_TAG_LEN - ( (i + (TAGE_COMP_NUM & 1)) / 2) );
+
+    tage_comp_list[i].second_tag.len_history = TAGE_HISTORY_LEN[i];
+    tage_comp_list[i].second_tag.compressed = 0;
+    tage_comp_list[i].second_tag.len_targe = (uint8_t) (TAGE_TAG_LEN - ( (i + (TAGE_COMP_NUM & 1)) / 2) - 1 );
+
+    tage_comp_list[i].index.len_history = TAGE_HISTORY_LEN[i];
+    tage_comp_list[i].index.compressed = 0;
+    tage_comp_list[i].index.len_targe = (uint8_t) my_log2(TAGE_COMP_ENTRY);
+
+    tage_comp_list[i].len_history = TAGE_HISTORY_LEN[i];
+  }
+
+}
+
+// predict
+
+uint8_t 
+tage_base_predict(uint32_t pc) {
+  uint32_t pc_lower_bits = pc & (TAGE_BASE_ENTRY-1);
+  uint32_t ghistory_lower_bits = tage_base_history & (TAGE_BASE_ENTRY -1);
+  uint32_t index = pc_lower_bits ^ ghistory_lower_bits;
+  switch(tage_base_gshare[index]){
+    case WN:
+      return NOTTAKEN;
+    case SN:
+      return NOTTAKEN;
+    case WT:
+      return TAKEN;
+    case ST:
+      return TAKEN;
+    default:
+      printf("Warning: Undefined state of entry in TAGE base GSHARE BHT!\n");
+      return NOTTAKEN;
+  }
+}
+
+uint8_t 
+tage_predict(uint32_t pc) {
+  uint8_t base_predict = tage_base_predict(pc);
+  return base_predict;
+}
+
+// train
+void
+train_tage_base(uint32_t pc, uint8_t outcome) {
+  //get lower ghistoryBits of pc
+  uint32_t pc_lower_bits = pc & (TAGE_BASE_ENTRY-1);
+  uint32_t ghistory_lower_bits = tage_base_history & (TAGE_BASE_ENTRY -1);
+  uint32_t index = pc_lower_bits ^ ghistory_lower_bits;
+
+  //Update state of entry in bht based on outcome
+  switch(tage_base_gshare[index]){
+    case WN:
+      tage_base_gshare[index] = (outcome==TAKEN)?WT:SN;
+      break;
+    case SN:
+      tage_base_gshare[index] = (outcome==TAKEN)?WN:SN;
+      break;
+    case WT:
+      tage_base_gshare[index] = (outcome==TAKEN)?ST:WN;
+      break;
+    case ST:
+      tage_base_gshare[index] = (outcome==TAKEN)?ST:WT;
+      break;
+    default:
+      printf("Warning: Undefined state of entry in TAGE base GSHARE BHT!\n");
+  }
+
+  //Update history register
+  tage_base_history = ((tage_base_history << 1) | outcome); 
+}
+
+void
+tage_train(uint32_t pc, uint8_t outcome) {
+  train_tage_base(pc, outcome);
+}
+
+
+//---------End of TAGE
+
 void
 init_predictor()
 {
@@ -356,6 +531,8 @@ init_predictor()
       init_tournament();
       break;
     case CUSTOM:
+      init_tage();
+      break;
     default:
       break;
   }
@@ -379,6 +556,7 @@ make_prediction(uint32_t pc)
     case TOURNAMENT:
       return tournament_predict(pc);
     case CUSTOM:
+      return tage_predict(pc);
     default:
       break;
   }
@@ -403,6 +581,7 @@ train_predictor(uint32_t pc, uint8_t outcome)
     case TOURNAMENT:
       return tournament_train(pc, outcome);
     case CUSTOM:
+      return tage_train(pc, outcome);
     default:
       break;
   }
